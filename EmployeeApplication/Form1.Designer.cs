b94738d48ae68f0f14ae5b5c8836d7fb5178fc63
Nico122803 <nicoadalim@gmail.com>
2024-10-16 22:08:41 +0800

    Add project files.

---
+++ EmployeeApplication/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Employee_application
+{
+    partial class frmEmployeeDatabase
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.EmployeeId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Position = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TextEmployeeId = new System.Windows.Forms.TextBox();
+            this.TextFirstName = new System.Windows.Forms.TextBox();
+            this.TextLastName = new System.Windows.Forms.TextBox();
+            this.TextPosition = new System.Windows.Forms.TextBox();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.SteelBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EmployeeId,
+            this.FirstName,
+            this.LastName,
+            this.Position});
+            this.dataGridView1.Location = new System.Drawing.Point(309, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(733, 341);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // EmployeeId
+            // 
+            this.EmployeeId.HeaderText = "ID";
+            this.EmployeeId.MinimumWidth = 6;
+            this.EmployeeId.Name = "EmployeeId";
+            this.EmployeeId.Width = 150;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.MinimumWidth = 6;
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 150;
+            // 
+            // LastName
+            // 
+            this.LastName.HeaderText = "LastName";
+            this.LastName.MinimumWidth = 6;
+            this.LastName.Name = "LastName";
+            this.LastName.Width = 150;
+            // 
+            // Position
+            // 
+            this.Position.HeaderText = "Position";
+            this.Position.MinimumWidth = 6;
+            this.Position.Name = "Position";
+            this.Position.Width = 150;
+            // 
+            // TextEmployeeId
+            // 
+            this.TextEmployeeId.Location = new System.Drawing.Point(14, 87);
+            this.TextEmployeeId.Name = "TextEmployeeId";
+            this.TextEmployeeId.Size = new System.Drawing.Size(287, 22);
+            this.TextEmployeeId.TabIndex = 1;
+            // 
+            // TextFirstName
+            // 
+            this.TextFirstName.Location = new System.Drawing.Point(14, 139);
+            this.TextFirstName.Name = "TextFirstName";
+            this.TextFirstName.Size = new System.Drawing.Size(287, 22);
+            this.TextFirstName.TabIndex = 2;
+            // 
+            // TextLastName
+            // 
+            this.TextLastName.Location = new System.Drawing.Point(14, 190);
+            this.TextLastName.Name = "TextLastName";
+            this.TextLastName.Size = new System.Drawing.Size(287, 22);
+            this.TextLastName.TabIndex = 3;
+            // 
+            // TextPosition
+            // 
+            this.TextPosition.Location = new System.Drawing.Point(14, 241);
+            this.TextPosition.Name = "TextPosition";
+            this.TextPosition.Size = new System.Drawing.Size(287, 22);
+            this.TextPosition.TabIndex = 4;
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAddEmployee.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAddEmployee.Location = new System.Drawing.Point(65, 286);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(168, 34);
+            this.btnAddEmployee.TabIndex = 5;
+            this.btnAddEmployee.Text = "Submit";
+            this.btnAddEmployee.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Add Employee:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Employee ID *";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "First Name * ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Last Name * ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Position * ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(309, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Employee List";
+            // 
+            // frmEmployeeDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(978, 439);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Controls.Add(this.TextPosition);
+            this.Controls.Add(this.TextLastName);
+            this.Controls.Add(this.TextFirstName);
+            this.Controls.Add(this.TextEmployeeId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmEmployeeDatabase";
+            this.Text = "Employee Database";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn EmployeeId;
+        private DataGridViewTextBoxColumn FirstName;
+        private DataGridViewTextBoxColumn LastName;
+        private DataGridViewTextBoxColumn Position;
+        private TextBox TextEmployeeId;
+        private TextBox TextFirstName;
+        private TextBox TextLastName;
+        private TextBox TextPosition;
+        private Button btnAddEmployee;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ EmployeeApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeNamespace;
+using System.Windows.Forms;
+using System;
+
+namespace Employee_application
+{
+    internal static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+
+            ApplicationConfiguration.Initialize();
+            Application.Run(new frmEmployeeDatabase());
+        }
+    }
+
+}
